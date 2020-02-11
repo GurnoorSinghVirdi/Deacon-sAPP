63ca791cc2d0e5fc4e4cc03149cbbac5e568662a
KyleSieb <KyleSieb@users.noreply.github.com>
2020-02-10 21:11:00 -0500

    project file

---
+++ Deacons/Deacons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Deacons
+//
+//  Created by Kyle Sieb on 2/10/20.
+//  Copyright © 2020 Sieb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
